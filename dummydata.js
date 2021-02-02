38c27d2fa179232b2ee9f2bd9b9b86587ffcb2cb
Ifeoluwapo123 <65716233+Ifeoluwapo123@users.noreply.github.com>
2021-02-02 04:36:35 -0800

    project setup

---
+++ dummydata.js
@@ -0,0 +1,24 @@
+const data = function () {
+  return [
+    {
+      userId: 123,
+      email: "xxx.gmail.com",
+      password: "xxx",
+      phone: "09090909090",
+    },
+    {
+      userId: 124,
+      email: "yyy.gmail.com",
+      password: "yyy",
+      phone: "07090909090",
+    },
+    {
+      userId: 125,
+      email: "zzz.gmail.com",
+      password: "zzz",
+      phone: "08090909090",
+    },
+  ];
+};
+
+module.exports = data;
