4ff612d7e25934bc8aa2fbd54e508ae3921ad900
Damilola <adenusidamilola5@gmail.com>
2021-03-18 03:04:45 -0700

    product service issue resolved

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,22 +1,22 @@
-import { Document } from "mongoose";
-import { IUser } from "../interfaces/entities";
-import User from "../models/User";
+import { Document } from 'mongoose'
+import { IUser } from '../interfaces/entities'
+import User from '../models/User'
 import {
   ICommentService,
   IFeedbackService,
   IPostService,
   IProductService,
   IUserService,
-} from "./interfaces";
-import { inject, injectable } from "inversify";
-import { NotFoundException, ServerErrorException } from "../exceptions";
-import UsersController from "../controllers/user.controller";
-import VerificationStatus from "../enums/VerificationStatus";
-import handleAppExceptions from "../utils/handleAppExceptions";
-import UnauthorizedException from "../exceptions/UnauthorizedException";
-import SavedAds from "../models/SavedAds";
-import Types from "../types";
-import Product from "../models/Product";
+} from './interfaces'
+import { inject, injectable } from 'inversify'
+import { NotFoundException, ServerErrorException } from '../exceptions'
+import UsersController from '../controllers/user.controller'
+import VerificationStatus from '../enums/VerificationStatus'
+import handleAppExceptions from '../utils/handleAppExceptions'
+import UnauthorizedException from '../exceptions/UnauthorizedException'
+import SavedAds from '../models/SavedAds'
+import Types from '../types'
+import Product from '../models/Product'
 
 @injectable()
 export default class UserService implements IUserService {
@@ -24,21 +24,21 @@ export default class UserService implements IUserService {
     @inject(Types.IPostService) private postService: IPostService,
     @inject(Types.ICommentService) private commentService: ICommentService,
     @inject(Types.IFeedbackService) private feedbackService: IFeedbackService,
-    @inject(Types.IProductService) private productService: IProductService
+    @inject(Types.IProductService) private productService: IProductService,
   ) {}
   async getUsers() {
     try {
-      return await User.find({}).populate("savedAds");
+      return await User.find({}).populate('savedAds')
     } catch (error) {
-      throw new ServerErrorException(error);
+      throw new ServerErrorException(error)
     }
   }
 
   async updateUser(id: string, entity: IUser) {
     try {
-      return await User.findByIdAndUpdate(id, entity);
+      return await User.findByIdAndUpdate(id, entity)
     } catch (error) {
-      throw new ServerErrorException(error);
+      throw new ServerErrorException(error)
     }
   }
 
@@ -46,89 +46,89 @@ export default class UserService implements IUserService {
     try {
       return await User.findByIdAndUpdate(id, {
         $set: { verificationStatus: status },
-      });
+      })
     } catch (error) {
-      throw new ServerErrorException(error);
+      throw new ServerErrorException(error)
     }
   }
 
   async getUser(userid: string) {
     try {
-      const user = await (await User.findById(userid)).toObject();
-      const userProfileData = await this.getUserProfileDetails(userid);
+      const user = await (await User.findById(userid)).toObject()
+      const userProfileData = await this.getUserProfileDetails(userid)
       return {
         ...user,
         ...userProfileData,
-      };
+      }
     } catch (error) {
-      throw ServerErrorException(error);
+      throw ServerErrorException(error)
     }
   }
 
   async getUserByCondition(query: IUser): Promise<Document<any>[]> {
     try {
-      return await User.find(query);
+      return await User.find(query)
     } catch (error) {
-      throw new ServerErrorException(error);
+      throw new ServerErrorException(error)
     }
   }
 
   async getByEmail(email: string): Promise<Document<IUser>> {
     try {
-      const user = await User.findOne({ email });
-      return user;
+      const user = await User.findOne({ email })
+      return user
     } catch (error) {
-      throw new ServerErrorException(error);
+      throw new ServerErrorException(error)
     }
   }
 
   async deleteUser(id: string): Promise<Document<any>> {
     try {
-      return await (await User.findById(id)).remove();
+      return await (await User.findById(id)).remove()
     } catch (error) {
-      throw new ServerErrorException(error);
+      throw new ServerErrorException(error)
     }
   }
 
   async saveAd(productid: string, userid: string): Promise<Document<any>> {
     try {
-      const user = await this.getUser(userid);
+      const user = await this.getUser(userid)
 
-      if (!user) throw new UnauthorizedException("You have to be logged In");
+      if (!user) throw new UnauthorizedException('You have to be logged In')
 
-      const product = await Product.findById(productid);
+      const product = await Product.findById(productid)
 
-      if (!product) throw new NotFoundException("ad not found");
+      if (!product) throw new NotFoundException('ad not found')
 
       const savedAd = await User.findByIdAndUpdate(user._id, {
         $addToSet: { savedAds: productid },
-      });
+      })
 
-      return await savedAd.save();
+      return await savedAd.save()
     } catch (error) {
-      console.log(error);
+      console.log(error)
 
-      throw error;
+      throw error
     }
   }
 
   //user profiles @dami
   private async getUserProfileDetails(userid) {
-    const userPosts = await this.postService.getPostsByUser(userid);
-    const userLikesOnPost = await this.postService.getPostsLikedByUser(userid);
+    const userPosts = await this.postService.getPostsByUser(userid)
+    const userLikesOnPost = await this.postService.getPostsLikedByUser(userid)
     const usersCommentsOnPost = await this.commentService.getCommentsByUser(
-      userid
-    );
+      userid,
+    )
     const userFeedbacks = await this.feedbackService.getFeedbacksSentByUser(
-      userid
-    );
+      userid,
+    )
     const allOtherFeedbacks = await this.feedbackService.getFeedbacksReceivedByUser(
-      userid
-    );
+      userid,
+    )
 
-    const receivedFeedbacks = await this.productService.getProductFeedbacksByUser(
-      userid
-    );
+    const receivedFeedbacks = await this.productService.getProductFeedbacks(
+      userid,
+    )
 
     return {
       userPosts,
@@ -136,6 +136,6 @@ export default class UserService implements IUserService {
       usersCommentsOnPost,
       userFeedbacks,
       receivedFeedbacks,
-    };
+    }
   }
 }
