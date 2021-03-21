22e7b45019573d76a23ed8a9c1c2c643856051d9
Damilola <adenusidamilola5@gmail.com>
2021-03-22 00:07:37 +0100

    added modified file that was mistakenly not included

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-import { Server, Socket } from 'socket.io'
-import SocketEvents from '../enums/SocketEvents'
-import { Chat, OnlineStatus, SocketError } from '../interfaces/SocketInterfaces'
-import helper from '../config/jwtHelper'
-import TokenContent from '../interfaces/TokenContent'
-import { container } from '../containerDI'
-import { UserService, MessageService } from '../services'
-import Types from '../types'
-
-const userService = container.get<UserService>(Types.IUserService)
-const messageService = container.get<MessageService>(Types.IMessageService)
-// helper to disconnect user
-const errorHappened = (socket: Socket, error: SocketError) => {
-  socket.emit(SocketEvents.Disconnected, error)
-}
-
-// helper to verify user
-const verifyUser = (socket, token): any => {
-  let user: any = {}
-  helper.verify(token, async (err, decoded) => {
-    if (err) {
-      console.log(err)
-      errorHappened(socket, {
-        statusCode: 401,
-        message: 'User is unauthorized',
-      })
-      socket.disconnect(true)
-    }
-
-    const userDetails = decoded as TokenContent
-    user = await userService.getUser(userDetails._id)
-  })
-  return user
-}
-
-const runConnection = (io: Server) => {
-  io.on('connection', (socket: Socket) => {
-    //Online
-    socket.on(SocketEvents.Online, async (response: OnlineStatus) => {
-      const user = await verifyUser(socket, response.token)
-      io.sockets.emit(SocketEvents.UserJoined, user)
-    })
-
-    socket.on(SocketEvents.SendMessage, async (response: Chat) => {
-      const user = await verifyUser(socket, response.token)
-      response.user = user
-      const saved = await messageService.saveMessage(response)
-      if (saved) {
-        io.sockets.emit(SocketEvents.ReceiveMessage, response)
-      } else {
-        errorHappened(socket, {
-          statusCode: 500,
-          message: 'Unable to save message',
-        })
-      }
-    })
-  })
-}
-=======
 import { Server, Socket } from "socket.io";
 import SocketEvents from "../enums/SocketEvents";
 import {
@@ -174,6 +114,5 @@ const runConnection = (io: Server) => {
     );
   });
 };
->>>>>>> c7dd9f5de37da2785a592c249a955c3ef98053cb
 
 export default runConnection;
