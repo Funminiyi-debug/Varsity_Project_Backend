e1359f23e927a1b924ddc4e7c4229d87fd9a1cc0
Funminiyi-debug <funminiyianwo@gmail.com>
2021-03-28 19:52:27 +0100

    built project

--- dist/enums/CategoryType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CategoryType;
-(function (CategoryType) {
-    CategoryType["Product"] = "Product";
-    CategoryType["Services"] = "Services";
-})(CategoryType || (CategoryType = {}));
-exports.default = CategoryType;
-//# sourceMappingURL=CategoryType.js.map
